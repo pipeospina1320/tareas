43dca96adbc02f6b4d235b5c1e0687185016245e
PipeOspina <juanfe-1994@hotmail.com>
2018-12-09 13:10:41 -0500

    primero commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err)
+            console.log('Error')
+        else
+            console.log('Guardado con exito')
+    })
+};
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+}
+
+const getLista = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const crear = (descripcion) => {
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+};
+
+
+module.exports = {
+    crear,
+    getLista,
+    actualizar
+}
